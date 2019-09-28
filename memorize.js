54fb1e69c17b0c3e7e48c2d655fc9629d86245b9
green bag <nlee9337@gmail.com>
2019-09-28 13:08:13 +0900

    remove storage item if necessary

--- memorize.js
+++ memorize.js
@@ -1,4 +1,16 @@
+const MAX_NUM_LIST_ITEM = 30;
+
 chrome.downloads.onCreated.addListener((downloadItem) => {
+  // 保存数が限界になったら古い順に削除していく
+  chrome.storage.local.get(null, removeFirstStorageItemIfNecessary);
+
   let key = dateFormat.format(new Date(), 'yyyyMMddhhmmss');
   chrome.storage.local.set({key: downloadItem.url});
 });
+
+const removeFirstStorageItemIfNecessary = (storageItems) => {
+  if (storageItems.length >= MAX_NUM_LIST_ITEM) {
+    let firstKey = Object.keys(storageItems)[0];
+    chrome.storage.local.remove(firstKey);
+  }
+}
