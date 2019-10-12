ff4b18be735c160213e519d528ad4ed77d56d353
green bag <nlee9337@gmail.com>
2019-10-13 00:16:07 +0900

    get items from storage and show table

--- re_download.js
+++ re_download.js
@@ -1,5 +1,28 @@
-$(function() {
-  $("#title").click(function() {
-    console.log("hello");
-  });
-});
+const makeTable = (datas) => {
+  $(document).ready(function () {
+    $("#content").append($("<table>")).append($("<tbody id='table-content'>"))
+
+    $.each(datas, (index, value) => {
+      $('tbody').append($(`<tr id='row-${index}'>`)
+        .append($(`<td id='td-${index}'>`).text(value))
+      )
+    })
+  })
+}
+
+const makeDefault = () => {
+  $('#content').append('<div>保存されたアイテムはありません。</div>')
+}
+
+const makeContents = () => {
+  chrome.storage.local.get(null, (items) => {
+    const len = Object.keys(items).length
+    if (len == 0) {
+      makeDefault()
+    } else {
+      makeTable(items)
+    }
+  })
+}
+
+document.onload = makeContents()
