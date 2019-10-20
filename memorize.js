1e44c43603f299bce5a1ba3cfea67698c222686c
green bag <nlee9337@gmail.com>
2019-10-21 05:07:33 +0900

    pass items to remove method

--- memorize.js
+++ memorize.js
@@ -2,7 +2,7 @@ const MAX_NUM_LIST_ITEM = 30;
 
 chrome.downloads.onCreated.addListener((downloadItem) => {
   // 保存数が限界になったら古い順に削除していく
-  chrome.storage.local.get(null, removeFirstStorageItemIfNecessary);
+  chrome.storage.local.get(null, (items) => removeFirstStorageItemIfNecessary(items));
 
   let storageKey = Date.now()
   chrome.storage.local.set({[storageKey]: downloadItem.url});
