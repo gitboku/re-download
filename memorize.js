0bd15b8a29a930c931ebb69a82917d4716267146
green bag <nlee9337@gmail.com>
2019-10-25 13:09:12 +0900

    add TODO comment

--- memorize.js
+++ memorize.js
@@ -1,5 +1,6 @@
 const MAX_NUM_LIST_ITEM = 30;
 
+// TODO: ダウンロードが完了したときに発火するようにする
 chrome.downloads.onCreated.addListener((downloadItem) => {
   // 保存数が限界になったら古い順に削除していく
   chrome.storage.local.get(null, (items) => removeFirstStorageItemIfNecessary(items));
